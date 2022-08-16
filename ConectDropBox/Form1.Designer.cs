c46d8ccbb111496beb923b28482a246638dcdede
AgnaldoSilva0 <agnaldo.santos24@hotmail.com>
2022-08-15 22:45:49 -0300

    Adicionar arquivos de projeto.

---
+++ ConectDropBox/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace ConectDropBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listLocal = new System.Windows.Forms.ListBox();
+            this.lbNome = new System.Windows.Forms.Label();
+            this.lbEmail = new System.Windows.Forms.Label();
+            this.lbPais = new System.Windows.Forms.Label();
+            this.btUpload = new System.Windows.Forms.Button();
+            this.btDowload = new System.Windows.Forms.Button();
+            this.listDropBox = new System.Windows.Forms.ListBox();
+            this.lbLocal = new System.Windows.Forms.Label();
+            this.lbDropBox = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listLocal
+            // 
+            this.listLocal.FormattingEnabled = true;
+            this.listLocal.Location = new System.Drawing.Point(12, 133);
+            this.listLocal.Name = "listLocal";
+            this.listLocal.Size = new System.Drawing.Size(269, 199);
+            this.listLocal.TabIndex = 0;
+            // 
+            // lbNome
+            // 
+            this.lbNome.AutoSize = true;
+            this.lbNome.Location = new System.Drawing.Point(13, 23);
+            this.lbNome.Name = "lbNome";
+            this.lbNome.Size = new System.Drawing.Size(35, 13);
+            this.lbNome.TabIndex = 1;
+            this.lbNome.Text = "Nome";
+            // 
+            // lbEmail
+            // 
+            this.lbEmail.AutoSize = true;
+            this.lbEmail.Location = new System.Drawing.Point(13, 46);
+            this.lbEmail.Name = "lbEmail";
+            this.lbEmail.Size = new System.Drawing.Size(35, 13);
+            this.lbEmail.TabIndex = 2;
+            this.lbEmail.Text = "Email:";
+            // 
+            // lbPais
+            // 
+            this.lbPais.AutoSize = true;
+            this.lbPais.Location = new System.Drawing.Point(13, 69);
+            this.lbPais.Name = "lbPais";
+            this.lbPais.Size = new System.Drawing.Size(27, 13);
+            this.lbPais.TabIndex = 3;
+            this.lbPais.Text = "Pais";
+            // 
+            // btUpload
+            // 
+            this.btUpload.Location = new System.Drawing.Point(105, 338);
+            this.btUpload.Name = "btUpload";
+            this.btUpload.Size = new System.Drawing.Size(75, 23);
+            this.btUpload.TabIndex = 4;
+            this.btUpload.Text = "Upload";
+            this.btUpload.UseVisualStyleBackColor = true;
+            this.btUpload.Click += new System.EventHandler(this.btUpload_Click);
+            // 
+            // btDowload
+            // 
+            this.btDowload.Location = new System.Drawing.Point(526, 338);
+            this.btDowload.Name = "btDowload";
+            this.btDowload.Size = new System.Drawing.Size(75, 23);
+            this.btDowload.TabIndex = 6;
+            this.btDowload.Text = "Download";
+            this.btDowload.UseVisualStyleBackColor = true;
+            this.btDowload.Click += new System.EventHandler(this.btDowload_Click);
+            // 
+            // listDropBox
+            // 
+            this.listDropBox.FormattingEnabled = true;
+            this.listDropBox.Location = new System.Drawing.Point(422, 133);
+            this.listDropBox.Name = "listDropBox";
+            this.listDropBox.Size = new System.Drawing.Size(269, 199);
+            this.listDropBox.TabIndex = 5;
+            // 
+            // lbLocal
+            // 
+            this.lbLocal.AutoSize = true;
+            this.lbLocal.Location = new System.Drawing.Point(12, 117);
+            this.lbLocal.Name = "lbLocal";
+            this.lbLocal.Size = new System.Drawing.Size(203, 13);
+            this.lbLocal.TabIndex = 7;
+            this.lbLocal.Text = "C:\\Users\\agnal\\Desktop\\TesteDropBox\\";
+            // 
+            // lbDropBox
+            // 
+            this.lbDropBox.AutoSize = true;
+            this.lbDropBox.Location = new System.Drawing.Point(419, 117);
+            this.lbDropBox.Name = "lbDropBox";
+            this.lbDropBox.Size = new System.Drawing.Size(55, 13);
+            this.lbDropBox.TabIndex = 8;
+            this.lbDropBox.Text = "/AgnSoft/";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(703, 369);
+            this.Controls.Add(this.lbDropBox);
+            this.Controls.Add(this.lbLocal);
+            this.Controls.Add(this.btDowload);
+            this.Controls.Add(this.listDropBox);
+            this.Controls.Add(this.btUpload);
+            this.Controls.Add(this.lbPais);
+            this.Controls.Add(this.lbEmail);
+            this.Controls.Add(this.lbNome);
+            this.Controls.Add(this.listLocal);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listLocal;
+        private System.Windows.Forms.Label lbNome;
+        private System.Windows.Forms.Label lbEmail;
+        private System.Windows.Forms.Label lbPais;
+        private System.Windows.Forms.Button btUpload;
+        private System.Windows.Forms.Button btDowload;
+        private System.Windows.Forms.ListBox listDropBox;
+        private System.Windows.Forms.Label lbLocal;
+        private System.Windows.Forms.Label lbDropBox;
+    }
+}
+
---
+++ ConectDropBox/Form1.cs
@@ -0,0 +1,144 @@
+﻿using Dropbox.Api;
+using Dropbox.Api.Files;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConectDropBox
+{
+    public partial class Form1 : Form
+    {
+        static string token = "????";
+        static string nome;
+        static string email;
+        static string pais;
+        static ArrayList entradas = new ArrayList();
+
+        static string source_folder;
+        static string target_folder;
+        static string file;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Listar Arquivos do Local
+            string[] arquivos = Directory.GetFiles(lbLocal.Text);
+            foreach (string arquivo in arquivos)
+                listLocal.Items.Add(Path.GetFileName(arquivo));
+
+            var task = Task.Run(Run);
+            try
+            {
+                task.Wait();
+                lbNome.Text = $"Nome: {nome}"; //Nome: " + nome
+                lbEmail.Text = $"Email: {email}";
+                lbPais.Text = $"Pais: {pais}";
+                foreach (string item in entradas)
+                {
+                    listDropBox.Items.Add(item);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Falha ao se conectar");
+            }
+        }
+
+        async Task Run()
+        {
+            using (var dbx = new DropboxClient(token))
+            {
+                var id = await dbx.Users.GetCurrentAccountAsync();
+                nome = id.Name.DisplayName;
+                email = id.Email;
+                pais = id.Country;
+
+                // mostrar as pastas
+                var folder = lbDropBox.Text;
+                try
+                {
+                    var list = await dbx.Files.ListFolderAsync(folder);
+                    foreach (var item in list.Entries.Where(i => i.IsFile))
+                        entradas.Add(item.Name);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Falha ao mostrar arquivos");
+                }
+            }
+        }
+
+        private void btUpload_Click(object sender, EventArgs e)
+        {
+            source_folder = lbLocal.Text;
+            target_folder = lbDropBox.Text;
+            file = listLocal.SelectedItem.ToString();
+
+            var task = Task.Run(Upload);
+            try
+            {
+                task.Wait();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Falha no Upload" + ex);
+            }
+        }
+
+        async Task Upload()
+        {
+            using (var dbx = new DropboxClient(token))
+            {
+                using (var content = new MemoryStream(File.ReadAllBytes(source_folder + file)))
+                {
+                    var update = await dbx.Files.UploadAsync(target_folder + file, WriteMode.Overwrite.Instance,
+                        body: content);
+
+                    var url = "https://www.dropbox.com/home/" + target_folder + file;
+                    MessageBox.Show("Upload Ralizado " + url);
+                }
+            }
+        }
+
+        private void btDowload_Click(object sender, EventArgs e)
+        {
+            source_folder = lbDropBox.Text;
+            target_folder = lbLocal.Text;
+            file = listDropBox.SelectedItem.ToString();
+
+            var task = Task.Run(Download);
+            try
+            {
+                task.Wait();
+                MessageBox.Show("Donwload " + listLocal.SelectedItem + " realizado");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Falha no Download" + ex);
+            }
+        }
+
+        async Task Download()
+        {
+            using (var dbx = new DropboxClient(token))
+            {
+                using (var response = await dbx.Files.DownloadAsync(source_folder + file))
+                {
+                    var content = response.GetContentAsByteArrayAsync();
+                    content.Wait();
+                    File.WriteAllBytes(target_folder + file, content.Result);
+                }
+            }
+            
+        }
+    }
+}
